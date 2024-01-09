b8c70b651f1c7affec271e39088ae63df5622b56
Abdullah Sheikh <sheikhabdullahkurana@gmail.com>
2024-01-09 12:22:24 +0500

    Component testing example added

--- project/__tests__/App.test.tsx
+++ project/__tests__/App.test.tsx
@@ -6,17 +6,19 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
+
+import { render, fireEvent } from '@testing-library/react-native';
+
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { Button } from 'react-native';
 
 it('renders correctly', () => {
   renderer.create(<App />);
 });
-
-
 
 
 
@@ -50,3 +52,39 @@ describe('filterArray function', () => {
     expect(result).toEqual([]);
   });
 });
+
+
+
+//Describing a test suite
+describe('ButtonComponent', () => {
+/*  in our case, let's say we have defined style for ButtonComponent, with padding==10, for component test, we test the style as well
+*/ 
+it('should render with text', async () => {
+  const { getByText } = render(<Button
+    title="Learn More"
+   
+  />);
+
+});
+ 
+  it('calls onPress when the button is pressed', () => {
+    // Mocking onPress method so we can check if its called or not
+    const onPressMock = jest.fn();
+    // Rendering Button component using react-native-test-renderer.
+    const { getByTestId } = render(<Button onPress={onPressMock} title="Click me" testID='button-1' />);
+    // Grabbing our button component to perform actions on it.
+    const button = getByTestId('button-1');
+    /**
+     * RNTL gives us API to fire events on node
+     * Here we are firing on press event
+     */
+    fireEvent.press(button);
+   // Asserting if given mock method is called or not.
+   
+    expect(onPressMock).toHaveBeenCalled();
+  });
+
+  
+
+});
+
